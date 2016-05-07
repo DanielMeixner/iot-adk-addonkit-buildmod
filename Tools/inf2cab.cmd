0be6219d63b6375d362b5b797dbd6a7e8f992295
Paramesh Babu Rengarao Thyada <paramesh.babu@microsoft.com>
2016-05-07 15:00:07 -0700

    Updated Inf2Cab to parse inf and extract dependencies
    Updated Inf2Cab to parse inf and extract dependencies

--- Tools/inf2cab.cmd
+++ Tools/inf2cab.cmd
@@ -19,7 +19,7 @@ if NOT DEFINED SRC_DIR (
 	exit /b -1
 )
 
-setlocal
+setlocal ENABLEDELAYEDEXPANSION
 
 if [%1] == [/?] goto Usage
 if [%1] == [-?] goto Usage
@@ -36,10 +36,24 @@ if [%PKG_TYPE%] ==[.inf] (
 
 
 :: Start processing command
+REM Extracting inf dependencies
+call :Extract_Dependency %1
 
 echo. Authoring %COMP_NAME%.%SUB_NAME%.pkg.xml
 :: Create Driver package using template files
-powershell -Command "(gc %IOTADK_ROOT%\Templates\DrvTemplate.pkg.xml) -replace 'COMPNAME', '%COMP_NAME%' -replace 'SUBNAME', '%SUB_NAME%' -replace 'DRIVER_NAME', '%FILE_NAME%' -replace 'PLFNAME', '%BSP_ARCH%' | Out-File %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml -Encoding utf8"
+powershell -Command "(gc %IOTADK_ROOT%\Templates\DrvTemplate1.pkg.xml) -replace 'COMPNAME', '%COMP_NAME%' -replace 'SUBNAME', '%SUB_NAME%' -replace 'DRIVER_NAME', '%FILE_NAME%' -replace 'PLFNAME', '%BSP_ARCH%' | Out-File %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml -Encoding utf8"
+
+echo. Updating Pkg xml with dependencies
+call :Update_PkgXml %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
+
+if exist %FILE_PATH%\temp.xml (
+del %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml 
+move /Y %FILE_PATH%\temp.xml %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml >nul
+REM Cleanup temp files
+del %FILE_PATH%\input.inf
+del %FILE_PATH%\infdep.txt
+)
+
 
 echo. Processing %COMP_NAME%.%SUB_NAME%.pkg.xml 
 set PKGBLD_DIR=%FILE_PATH%\
@@ -57,3 +71,65 @@ exit /b 1
 :End
 endlocal
 exit /b 0
+
+:Extract_Dependency
+setlocal ENABLEDELAYEDEXPANSION
+set TOKEN=0
+if exist %FILE_PATH%\infdep.txt ( del %FILE_PATH%\infdep.txt )
+if exist %FILE_PATH%\input.inf ( del %FILE_PATH%\input.inf )
+echo. Processing %1
+REM Convert the encoding format to utf8
+powershell -Command "(gc %1) | Out-File %FILE_PATH%\input.inf -Encoding utf8"
+REM Parse the inf section SourceDiskFiles and get the list of dependencies
+for /f "delims=" %%i in (%FILE_PATH%\input.inf) do (
+   if !TOKEN! == 1 (
+		for /f "tokens=1,* delims= " %%A in ("%%i") do (
+			if "%%B" EQU "" (
+					set TOKEN=0
+				) else (
+				 echo.%%A>> %FILE_PATH%\infdep.txt
+				)
+		)
+    )
+   if "%%i" EQU "[SourceDisksFiles]" (
+    set TOKEN=1
+   )	  
+)
+endlocal
+exit /b
+
+:Update_PkgXml
+setlocal ENABLEDELAYEDEXPANSION
+if exist %FILE_PATH%\temp.xml (del %FILE_PATH%\temp.xml)
+if not exist %FILE_PATH%\infdep.txt (
+	echo. error, file not found :%FILE_PATH%\infdep.txt 
+	exit /b 1
+)
+set TOKEN=0
+for /f "delims=" %%i in (%1) do (
+   if !TOKEN! == 1 (
+		for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
+			call :PRINT_TEXT "         <Reference Source="%%A" />"
+		)
+		call :PRINT_TEXT "         <Files>"
+		
+		for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
+			call :PRINT_TEXT "           <File Source="%%A" />"
+		)
+		call :PRINT_TEXT "         </Files>"		
+		set TOKEN=0
+    )
+   if "%%i" EQU "RefMark_Start" (
+		call :PRINT_TEXT "      <Driver InfSource="%FILE_NAME%.inf">"
+        set TOKEN=1
+   ) else (
+		echo %%i >> %FILE_PATH%\temp.xml
+   ) 	  
+)
+endlocal
+exit /b 0
+
+:PRINT_TEXT
+for /f "useback tokens=*" %%a in ('%1') do set TEXT=%%~a
+echo !TEXT! >> %FILE_PATH%\temp.xml
+exit /b
