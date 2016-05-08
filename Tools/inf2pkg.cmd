c342b2fa804cbe87a118ed8e20fb5d9e307e8615
Paramesh Babu Rengarao Thyada <paramesh.babu@microsoft.com>
2016-05-08 11:17:28 -0700

    Added appx2pkg.cmd to create pkg file for appx
    appx2pkg.cmd creates pkg file for the provided appx. Expects the
    dependencies in the subfolder.

--- Tools/inf2pkg.cmd
+++ Tools/inf2pkg.cmd
@@ -20,9 +20,11 @@ setlocal ENABLEDELAYEDEXPANSION
 if [%1] == [/?] goto Usage
 if [%1] == [-?] goto Usage
 if [%1] == [] goto Usage
+if not [%~x1] == [.inf] goto Usage
 
 set FILE_NAME=%~n1
 set FILE_PATH=%~dp1
+set OUTPUT_PATH=%FILE_PATH%
 if [%2] == [] (
 	set COMP_NAME=Drivers
 	set SUB_NAME=%FILE_NAME%
@@ -38,25 +40,27 @@ REM Extracting inf dependencies
 call :Extract_Dependency %1
 
 echo. Authoring %COMP_NAME%.%SUB_NAME%.pkg.xml
-if exist "%FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml" (del "%FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml" )
+if exist "%OUTPUT_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml" (del "%OUTPUT_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml" )
 call :Create_PkgXml 
 
 REM check for dependency files in the same folder and flag error if missing
-for /f "delims=" %%i in (%FILE_PATH%\infdep.txt) do (
-	if not exist "%FILE_PATH%%%i"  echo.   Warning : %FILE_PATH%%%i not found
+for /f "delims=" %%i in (%FILE_PATH%\inf_filelist.txt) do (
+	if not exist "%FILE_PATH%%%i" (
+	    echo.   Warning : %FILE_PATH%%%i not found, package creation will fail. 		
+	)
+	
 )	
 
 REM Cleanup temp files
 del %FILE_PATH%\input.inf
-del %FILE_PATH%\infdep.txt
-
+REM del %FILE_PATH%\inf_filelist.txt
 
 endlocal
 exit /b 0
 
 :Extract_Dependency
 set TOKEN=0
-if exist %FILE_PATH%\infdep.txt ( del %FILE_PATH%\infdep.txt )
+if exist %FILE_PATH%\inf_filelist.txt ( del %FILE_PATH%\inf_filelist.txt )
 if exist %FILE_PATH%\input.inf ( del %FILE_PATH%\input.inf )
 echo. Processing %1
 REM Convert the encoding format to utf8
@@ -69,7 +73,7 @@ for /f "delims=" %%i in (%FILE_PATH%\input.inf) do (
 					set TOKEN=0
 					exit /b
 				) else (
-				 echo.%%A>> %FILE_PATH%\infdep.txt
+				 echo.%%A>> %FILE_PATH%\inf_filelist.txt
 				)
 		)
     )
@@ -80,24 +84,24 @@ for /f "delims=" %%i in (%FILE_PATH%\input.inf) do (
 exit /b
 
 :Create_PkgXml
-if not exist %FILE_PATH%\infdep.txt (
-	echo. error, file not found :%FILE_PATH%\infdep.txt 
+if not exist %FILE_PATH%\inf_filelist.txt (
+	echo. error, file not found :%FILE_PATH%\inf_filelist.txt 
 	exit /b 1
 )
 REM Printing the headers
 call :PRINT_TEXT "<?xml version="1.0" encoding="utf-8" ?>" 
 call :PRINT_TEXT "<Package xmlns="urn:Microsoft.WindowsPhone/PackageSchema.v8.00""
-echo          Owner="$(OEMNAME)" OwnerType="OEM" ReleaseType="Production" >> %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
+echo          Owner="$(OEMNAME)" OwnerType="OEM" ReleaseType="Production" >> %OUTPUT_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
 call :PRINT_TEXT "         Platform="%BSP_ARCH%" Component="%COMP_NAME%" SubComponent="%SUB_NAME%">"
 call :PRINT_TEXT "   <Components>"
 call :PRINT_TEXT "      <Driver InfSource="%FILE_NAME%.inf">"
 REM Printing references
-for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
+for /f "delims=" %%A in (%FILE_PATH%\inf_filelist.txt) do (
 	call :PRINT_TEXT "         <Reference Source="%%A" />"
 )
 call :PRINT_TEXT "         <Files>"
 REM Printing file sources
-for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
+for /f "delims=" %%A in (%FILE_PATH%\inf_filelist.txt) do (
 	call :PRINT_TEXT "           <File Source="%%A" />"
 )
 call :PRINT_TEXT "         </Files>"
@@ -109,5 +113,5 @@ exit /b 0
 
 :PRINT_TEXT
 for /f "useback tokens=*" %%a in ('%1') do set TEXT=%%~a
-echo !TEXT! >> %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
+echo !TEXT! >> %OUTPUT_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
 exit /b
