d3c7433f96c46221871c8f2b7216e549dafdd07e
Paramesh Babu Rengarao Thyada <paramesh.babu@microsoft.com>
2016-05-07 20:06:10 -0700

    introduced inf2pkg.cmd
    introduced inf2pkg.cmd and updated inf2cab.cmd to use inf2pkg.cmd

--- Tools/inf2cab.cmd
+++ Tools/inf2cab.cmd
@@ -5,9 +5,10 @@
 goto START
 
 :Usage
-echo Usage: inf2cab filename.inf 
-echo    filename.inf..... Required, input .inf file; expects filename.sys file in the same location 
-echo    [/?]............. Displays this usage string. 
+echo Usage: inf2cab filename.inf [CompName.SubCompName]
+echo    filename.inf............ Required, input .inf file
+echo    CompName.SubCompName.... Optional, default is Drivers.filename 
+echo    [/?].................... Displays this usage string. 
 echo    Example:
 echo        inf2cab C:\test\testdriver.inf
 exit /b 1
@@ -19,41 +20,23 @@ if NOT DEFINED SRC_DIR (
 	exit /b -1
 )
 
-setlocal ENABLEDELAYEDEXPANSION
-
 if [%1] == [/?] goto Usage
 if [%1] == [-?] goto Usage
 if [%1] == [] goto Usage
 
-set PKG_TYPE=%~x1
 set FILE_NAME=%~n1
 set FILE_PATH=%~dp1
-
-if [%PKG_TYPE%] ==[.inf] (
+if [%2] == [] (
 	set COMP_NAME=Drivers
 	set SUB_NAME=%FILE_NAME%
-) else ( goto Usage )
-
-
-:: Start processing command
-REM Extracting inf dependencies
-call :Extract_Dependency %1
-
-echo. Authoring %COMP_NAME%.%SUB_NAME%.pkg.xml
-:: Create Driver package using template files
-powershell -Command "(gc %IOTADK_ROOT%\Templates\DrvTemplate1.pkg.xml) -replace 'COMPNAME', '%COMP_NAME%' -replace 'SUBNAME', '%SUB_NAME%' -replace 'DRIVER_NAME', '%FILE_NAME%' -replace 'PLFNAME', '%BSP_ARCH%' | Out-File %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml -Encoding utf8"
-
-echo. Updating Pkg xml with dependencies
-call :Update_PkgXml %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
-
-if exist %FILE_PATH%\temp.xml (
-del %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml 
-move /Y %FILE_PATH%\temp.xml %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml >nul
-REM Cleanup temp files
-del %FILE_PATH%\input.inf
-del %FILE_PATH%\infdep.txt
+) else (
+	for /f "tokens=1,2 delims=." %%i in ("%2") do ( 
+		set COMP_NAME=%%i
+		set SUB_NAME=%%j
+	)
 )
 
+call inf2pkg.cmd %1 %COMP_NAME%.%SUB_NAME%
 
 echo. Processing %COMP_NAME%.%SUB_NAME%.pkg.xml 
 set PKGBLD_DIR=%FILE_PATH%\
@@ -72,64 +55,3 @@ exit /b 1
 endlocal
 exit /b 0
 
-:Extract_Dependency
-setlocal ENABLEDELAYEDEXPANSION
-set TOKEN=0
-if exist %FILE_PATH%\infdep.txt ( del %FILE_PATH%\infdep.txt )
-if exist %FILE_PATH%\input.inf ( del %FILE_PATH%\input.inf )
-echo. Processing %1
-REM Convert the encoding format to utf8
-powershell -Command "(gc %1) | Out-File %FILE_PATH%\input.inf -Encoding utf8"
-REM Parse the inf section SourceDiskFiles and get the list of dependencies
-for /f "delims=" %%i in (%FILE_PATH%\input.inf) do (
-   if !TOKEN! == 1 (
-		for /f "tokens=1,* delims= " %%A in ("%%i") do (
-			if "%%B" EQU "" (
-					set TOKEN=0
-				) else (
-				 echo.%%A>> %FILE_PATH%\infdep.txt
-				)
-		)
-    )
-   if "%%i" EQU "[SourceDisksFiles]" (
-    set TOKEN=1
-   )	  
-)
-endlocal
-exit /b
-
-:Update_PkgXml
-setlocal ENABLEDELAYEDEXPANSION
-if exist %FILE_PATH%\temp.xml (del %FILE_PATH%\temp.xml)
-if not exist %FILE_PATH%\infdep.txt (
-	echo. error, file not found :%FILE_PATH%\infdep.txt 
-	exit /b 1
-)
-set TOKEN=0
-for /f "delims=" %%i in (%1) do (
-   if !TOKEN! == 1 (
-		for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
-			call :PRINT_TEXT "         <Reference Source="%%A" />"
-		)
-		call :PRINT_TEXT "         <Files>"
-		
-		for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
-			call :PRINT_TEXT "           <File Source="%%A" />"
-		)
-		call :PRINT_TEXT "         </Files>"		
-		set TOKEN=0
-    )
-   if "%%i" EQU "RefMark_Start" (
-		call :PRINT_TEXT "      <Driver InfSource="%FILE_NAME%.inf">"
-        set TOKEN=1
-   ) else (
-		echo %%i >> %FILE_PATH%\temp.xml
-   ) 	  
-)
-endlocal
-exit /b 0
-
-:PRINT_TEXT
-for /f "useback tokens=*" %%a in ('%1') do set TEXT=%%~a
-echo !TEXT! >> %FILE_PATH%\temp.xml
-exit /b
---
+++ Tools/inf2pkg.cmd
@@ -0,0 +1,113 @@
+:: Run setenv before running this script
+:: This script creates the folder structure and copies the template files for a new package
+@echo off
+
+goto START
+
+:Usage
+echo Usage: inf2pkg input.inf [CompName.SubCompName]
+echo    input.inf............... Required, input .inf file
+echo    CompName.SubCompName.... Optional, default is Drivers.input
+echo    [/?].................... Displays this usage string.
+echo    Example:
+echo        inf2pkg C:\test\testdriver.inf 
+exit /b 1
+
+:START
+
+setlocal ENABLEDELAYEDEXPANSION
+
+if [%1] == [/?] goto Usage
+if [%1] == [-?] goto Usage
+if [%1] == [] goto Usage
+
+set FILE_NAME=%~n1
+set FILE_PATH=%~dp1
+if [%2] == [] (
+	set COMP_NAME=Drivers
+	set SUB_NAME=%FILE_NAME%
+) else (
+	for /f "tokens=1,2 delims=." %%i in ("%2") do ( 
+		set COMP_NAME=%%i
+		set SUB_NAME=%%j
+	)
+)
+
+REM Start processing command
+REM Extracting inf dependencies
+call :Extract_Dependency %1
+
+echo. Authoring %COMP_NAME%.%SUB_NAME%.pkg.xml
+if exist "%FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml" (del "%FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml" )
+call :Create_PkgXml 
+
+REM check for dependency files in the same folder and flag error if missing
+for /f "delims=" %%i in (%FILE_PATH%\infdep.txt) do (
+	if not exist "%FILE_PATH%%%i"  echo.   Warning : %FILE_PATH%%%i not found
+)	
+
+REM Cleanup temp files
+del %FILE_PATH%\input.inf
+del %FILE_PATH%\infdep.txt
+
+
+endlocal
+exit /b 0
+
+:Extract_Dependency
+set TOKEN=0
+if exist %FILE_PATH%\infdep.txt ( del %FILE_PATH%\infdep.txt )
+if exist %FILE_PATH%\input.inf ( del %FILE_PATH%\input.inf )
+echo. Processing %1
+REM Convert the encoding format to utf8
+powershell -Command "(gc %1) | Out-File %FILE_PATH%\input.inf -Encoding utf8"
+REM Parse the inf section SourceDiskFiles and get the list of dependencies
+for /f "delims=" %%i in (%FILE_PATH%\input.inf) do (
+   if !TOKEN! == 1 (
+		for /f "tokens=1,* delims= " %%A in ("%%i") do (
+			if "%%B" EQU "" (
+					set TOKEN=0
+					exit /b
+				) else (
+				 echo.%%A>> %FILE_PATH%\infdep.txt
+				)
+		)
+    )
+   if "%%i" EQU "[SourceDisksFiles]" (
+    set TOKEN=1
+   )	  
+)
+exit /b
+
+:Create_PkgXml
+if not exist %FILE_PATH%\infdep.txt (
+	echo. error, file not found :%FILE_PATH%\infdep.txt 
+	exit /b 1
+)
+REM Printing the headers
+call :PRINT_TEXT "<?xml version="1.0" encoding="utf-8" ?>" 
+call :PRINT_TEXT "<Package xmlns="urn:Microsoft.WindowsPhone/PackageSchema.v8.00""
+echo          Owner="$(OEMNAME)" OwnerType="OEM" ReleaseType="Production" >> %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
+call :PRINT_TEXT "         Platform="%BSP_ARCH%" Component="%COMP_NAME%" SubComponent="%SUB_NAME%">"
+call :PRINT_TEXT "   <Components>"
+call :PRINT_TEXT "      <Driver InfSource="%FILE_NAME%.inf">"
+REM Printing references
+for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
+	call :PRINT_TEXT "         <Reference Source="%%A" />"
+)
+call :PRINT_TEXT "         <Files>"
+REM Printing file sources
+for /f "delims=" %%A in (%FILE_PATH%\infdep.txt) do (
+	call :PRINT_TEXT "           <File Source="%%A" />"
+)
+call :PRINT_TEXT "         </Files>"
+call :PRINT_TEXT "      </Driver>"
+call :PRINT_TEXT "   </Components>" 
+call :PRINT_TEXT "</Package>" 		
+)
+exit /b 0
+
+:PRINT_TEXT
+for /f "useback tokens=*" %%a in ('%1') do set TEXT=%%~a
+echo !TEXT! >> %FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml
+exit /b
