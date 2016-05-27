0a90cd07d822f73735d28f5a923fecd70c6672a3
Paramesh Babu Rengarao Thyada <paramesh.babu@microsoft.com>
2016-05-27 15:43:17 -0700

    Support for x64 added
    Support for x64 added
    BSP for MBMx64

---
+++ Source-x64/Packages/Appx.TextGlowClock/AppInstall/AppxConfig.cmd
@@ -0,0 +1,3 @@
+set AppxName=TextGlowClock_1.0.0.0_x64
+set certslist=TextGlowClock_1.0.0.0_x64  
+set dependencylist=Microsoft.NET.Native.Framework.1.3 Microsoft.NET.Native.Runtime.1.3 Microsoft.VCLibs.x64.14.00  
--- Tools/LaunchTool.cmd
+++ Tools/LaunchTool.cmd
@@ -1,55 +1,55 @@
 @echo off
 
-REM Set IOTADK_ROOT 
+REM Set IOTADK_ROOT
 set IOTADK_ROOT=%~dp0
 REM Getting rid of the \Tools\ at the end
 set IOTADK_ROOT=%IOTADK_ROOT:~0,-7%
 
 REM Get the Kits Root path from registry
 for /F "skip=2 tokens=2,*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows Kits\Installed Roots" /v KitsRoot10') do (
-	set KITPATH=%%BAssessment and Deployment Kit\Deployment Tools
+    set KITPATH=%%BAssessment and Deployment Kit\Deployment Tools
 )
 REM Check for ADK Presence and Launch
 if exist "%KITPATH%\DandISetEnv.bat" (
-	call "%KITPATH%\DandISetEnv.bat"
+    call "%KITPATH%\DandISetEnv.bat"
 ) else (
-	echo.
-	echo.Error : ADK not found. Please install ADK.
-	echo.
-	pause
-	exit /b
+    echo.
+    echo.Error : ADK not found. Please install ADK.
+    echo.
+    pause
+    exit /b
 )
 REM Remove temporary variables
-set KITPATH= 
+set KITPATH=
 
 REM Check for WDK Presence
 if exist "%KITSROOT%\CoreSystem" (
-	dir /B /AD "%KITSROOT%CoreSystem" > %IOTADK_ROOT%\wdkversion.txt
-	set /P WDK_VERSION=<%IOTADK_ROOT%\wdkversion.txt
-	del %IOTADK_ROOT%\wdkversion.txt
+    dir /B /AD "%KITSROOT%CoreSystem" > %IOTADK_ROOT%\wdkversion.txt
+    set /P WDK_VERSION=<%IOTADK_ROOT%\wdkversion.txt
+    del %IOTADK_ROOT%\wdkversion.txt
 ) else (
-	set WDK_VERSION=NotFound
+    set WDK_VERSION=NotFound
 
 )
 
 REM Check for Corekit packages
 if exist "%KITSROOT%\MSPackages" (
-	REM Get version number of the Corekit packages installed
-	reg query "HKEY_CLASSES_ROOT\Installer\Dependencies\Microsoft.Windows.Windows_10_IoT_Core_x86_Packages.x86.10" /v Version > %IOTADK_ROOT%\corekitversion.txt 2>nul
-	if errorlevel 1 ( 
-		REM MSPackages present without this registry key - Assuming older version of packages.
-		set COREKIT_VER=10586.0
-	) else ( 
-		for /F "skip=2 tokens=3" %%r in (%IOTADK_ROOT%\corekitversion.txt) do ( set KIT_VERSION=%%r )
-	)
-	del %IOTADK_ROOT%\corekitversion.txt	
+    REM Get version number of the Corekit packages installed
+    reg query "HKEY_CLASSES_ROOT\Installer\Dependencies\Microsoft.Windows.Windows_10_IoT_Core_x86_Packages.x86.10" /v Version > %IOTADK_ROOT%\corekitversion.txt 2>nul
+    if errorlevel 1 (
+        REM MSPackages present without this registry key - Assuming older version of packages.
+        set COREKIT_VER=10586.0
+    ) else (
+        for /F "skip=2 tokens=3" %%r in (%IOTADK_ROOT%\corekitversion.txt) do ( set KIT_VERSION=%%r )
+    )
+    del %IOTADK_ROOT%\corekitversion.txt
 ) else (
-	set KIT_VERSION=NotFound
-	set COREKIT_VER=NotFound
-	echo.Warning : Core kit packages not found. Image creation will fail.
+    set KIT_VERSION=NotFound
+    set COREKIT_VER=NotFound
+    echo.Warning : Core kit packages not found. Image creation will fail.
 )
-if defined KIT_VERSION ( 
-	for /f "tokens=2,* delims=." %%A in ("%KIT_VERSION%") do ( set COREKIT_VER=%%B ) 
+if defined KIT_VERSION (
+    for /f "tokens=2,* delims=." %%A in ("%KIT_VERSION%") do ( set COREKIT_VER=%%B )
 )
 
 set PATH=%PATH%;%IOTADK_ROOT%\Tools;
@@ -63,12 +63,14 @@ echo WDK_VERSION : %WDK_VERSION%
 echo COREKIT_VER : %COREKIT_VER%
 echo OEM_NAME    : %OEM_NAME%
 echo.
-echo Set Environment for Architecture 
-choice /C 12 /N /M "Choose 1 for ARM and 2 for x86"
+echo Set Environment for Architecture
+choice /C 123 /N /M "Choose 1 for ARM, 2 for x86 and 3 for x64 :"
 echo.
-if ERRORLEVEL 2 (
-call setenv x86
-)else if ERRORLEVEL 1 (
-call setenv arm 
+if errorlevel 3 (
+    call setenv x64
+)else if errorlevel 2 (
+    call setenv x86
+)else if errorlevel 1 (
+    call setenv arm
 )
 
