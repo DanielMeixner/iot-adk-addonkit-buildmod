41f9068eac6873f8c0b754eeb9a3760da828af62
Paramesh Babu <pabab@microsoft.com>
2016-06-28 10:47:22 -0700

    Enabling multiple apps installation using OEMTools\InstallAppx
    Enabling multiple apps installation using OEMTools\InstallAppx

--- Templates/oemcustomization.cmd
+++ Templates/oemcustomization.cmd
@@ -5,6 +5,11 @@ REM This script if included in the image, is called everytime the system boots.
 REM Enable Administrator User
 net user Administrator p@ssw0rd /active:yes
 
+if exist C:\OEMTools\InstallAppx.cmd (
+    REM Run the Appx Installer. This will install the appx present in C:\OEMApps\
+    call C:\OEMTools\InstallAppx.cmd
+)
+
 REM if exist C:\AppInstall\AppInstall.cmd (
 REM     REM Enable Application Installation for onetime only, after this the files are deleted.
 REM     call C:\AppInstall\AppInstall.cmd > %temp%\AppInstallLog.txt
