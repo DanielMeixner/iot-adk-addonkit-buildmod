f7b70e7c5d0f8b0b605f8099d6dfc5429a6649c0
Paramesh Babu Rengarao Thyada <paramesh.babu@microsoft.com>
2016-07-27 10:37:06 -0700

    Update to BuildKitSamples cmd
    BuildKitSamples updated

--- Tools/BuildKitAgent.cmd
+++ Tools/BuildKitAgent.cmd
@@ -1,15 +1,17 @@
-REM This script builds FFUs for all OEMInputSamples in Core Kit package
-
 @echo off
+
+REM This script builds FFUs for all OEMInputSamples in Core Kit package
 
 call %~dp0\LaunchTool.cmd arm
 
 REM call setenv arm
 echo Building arm sample FFUs
-call BuildKitSamples.cmd
+call BuildKitSamples.cmd Test
+call BuildKitSamples.cmd Retail
 
 call setenv x86
 echo Building x86 sample FFUs
-call BuildKitSamples.cmd
+call BuildKitSamples.cmd Test
+call BuildKitSamples.cmd Retail
 
 exit /b 0
--- Tools/BuildKitSamples.cmd
+++ Tools/BuildKitSamples.cmd
@@ -1,22 +1,36 @@
-REM This script builds architecture specific FFUs for OEMInputSamples in Core Kit package
-REM Usage: BuildKitSamples [BuildType]
-REM    BuildType......... Optional, Retail/Test; if not specified, it builds both Retail and Test
-REM    Example:
-REM        BuildKitSamples Test
-REM        BuildKitSamples Retail
-REM        BuildKitSamples 
-
 @echo off
+REM This script builds architecture specific FFUs for OEMInputSamples in Core Kit package
 setlocal
+
+goto START
+
+:Usage
+echo Usage: BuildKitSamples [BuildType]
+echo    BuildType.........  Retail/Test 
+echo    Example:
+echo        BuildKitSamples Test
+echo        BuildKitSamples Retail
+
+exit /b 1
+
+:START
+REM Input validation
+if [%1] == [/?] goto Usage
+if [%1] == [-?] goto Usage
+if [%1] == [] goto Usage
+
+if /I NOT [%1] == [Retail] ( if /I NOT [%1] == [Test] goto Usage  )
 set FLAVOUR=%1
-if /I NOT [%1] == [Retail] ( if /I NOT [%1] == [Test] set FLAVOUR=  )
+
 echo.
 echo.Build Start Time : %TIME%
 echo.
 
 if [%ARCH%] == [arm] (
+	echo Building RPi2 product 
 	call :COPY_AND_BUILD RPi2 %FLAVOUR%
 ) else if [%ARCH%] == [x86] (
+	echo Building MBM product
 	call :COPY_AND_BUILD MBM %FLAVOUR%
 )
 
@@ -33,8 +47,8 @@ if not exist "%PRODUCT_DIR%" (
 	mkdir "%PRODUCT_DIR%"
 )
 echo.Copying %1 OEMInputSamples to %PRODUCT_DIR%
-copy "%KITSROOT%OEMInputSamples\%1\RetailOemInput.xml" "%PRODUCT_DIR%\RetailOemInput.xml" >nul
-copy "%KITSROOT%OEMInputSamples\%1\ProductionOemInput.xml" "%PRODUCT_DIR%\TestOemInput.xml" >nul
+copy "%KITSROOT%OEMInputSamples\%BSP_ARCH%\%1\RetailOemInput.xml" "%PRODUCT_DIR%\RetailOemInput.xml" >nul
+copy "%KITSROOT%OEMInputSamples\%BSP_ARCH%\%1\ProductionOemInput.xml" "%PRODUCT_DIR%\TestOemInput.xml" >nul
 echo.Building %1FFU %2
 call buildimage %1FFU %2
 exit /b 0
