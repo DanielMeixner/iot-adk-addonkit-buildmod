3d1ff4c22547e3f85433f9a4461fecd4910cfdcd
Paramesh Babu Rengarao Thyada <paramesh.babu@microsoft.com>
2016-08-26 14:08:59 -0700

    Improving buildprovpkg and removing ppkg from repo
    Improving buildprovpkg and removing ppkg from repo

--- Tools/buildprovpkg.cmd
+++ Tools/buildprovpkg.cmd
@@ -3,16 +3,18 @@
 goto START
 
 :Usage
-echo Usage: buildprovpkg [CompName.SubCompName]/[All]/Clean
-echo    CompName.SubCompName...... Package ComponentName.SubComponent Name
-echo    All....................... All prov packages under \Packages directory are built
+echo Usage: buildprovpkg [CompName.SubCompName]/[ProductName]/[All]/Clean
+echo    CompName.SubCompName...... Package Name under Packages directory
+echo    ProductName............... Product Name under Products directory
+echo    All....................... All prov packages under \Packages directory and \Products directory are built
 echo    Clean..................... Removes all ppkg files
 echo        One of the above should be specified
-echo    [version]................. Optional, Package version. If not specified, it uses BSP_VERSION
 echo    [/?]...................... Displays this usage string.
 echo    Example:
 echo        buildprovpkg Provisioning.Enroll
-echo        buildpkg All
+echo        buildprovpkg SampleA
+echo        buildprovpkg All
+echo        buildprovpkg Clean
 
 exit /b 1
 
@@ -29,29 +31,25 @@ if [%1] == [-?] goto Usage
 if [%1] == [] goto Usage
 
 if /I [%1] == [All] (
-    echo Creating all provisioning packages under %COMMON_PKG%
+    echo Processing all provisioning packages
     cd %COMMON_PKG%
-    dir Provisioning.* /b /AD  > %PKGLOG_DIR%\commonprovlist.txt
+    dir /b /AD  > %PKGLOG_DIR%\commonprovlist.txt
+    cd %PKGSRC_DIR%
+    dir /b /AD  >> %PKGLOG_DIR%\commonprovlist.txt
+    cd %PRODUCTS_DIR%
+    dir /b /AD  >> %PKGLOG_DIR%\commonprovlist.txt
+
     for /f "delims=" %%i in (%PKGLOG_DIR%\commonprovlist.txt) do (
         call :SUB_PROCESSLIST %%i
     )
-    echo Creating all provisioning packages under %PRODUCTS_DIR%
-    cd %PRODUCTS_DIR%
-    dir /b /AD  > %PKGLOG_DIR%\commonprovlist.txt
-    for /f "delims=" %%i in (%PKGLOG_DIR%\commonprovlist.txt) do (
-        if exist "%PRODUCTS_DIR%\%%i\prov\customizations.xml" (
-            echo. Processing %%i
-            call createprovpkg.cmd %PRODUCTS_DIR%\%%i\prov\customizations.xml %PRODUCTS_DIR%\%%i\prov\%%iProv.ppkg > %PKGLOG_DIR%\%%i.prov.log
-        ) else (
-            echo. Skipping %%i
-        )
-    )
     del %PKGLOG_DIR%\commonprovlist.txt
 ) else if /I [%1] == [Clean] (
-	del %COMMON_DIR%\*.ppkg /S /Q
-	del %SRC_DIR%\*.ppkg /S /Q
+    del /S /Q %COMMON_DIR%\*.ppkg %COMMON_DIR%\*.cat >nul 2>nul
+    del /S /Q %PKGSRC_DIR%\*.ppkg %PKGSRC_DIR%\*.cat >nul 2>nul
+    del /S /Q %PRODUCTS_DIR%\*.ppkg %PRODUCTS_DIR%\*.cat >nul 2>nul
+    echo.All provisioning files cleaned.
 ) else (
-    call :SUB_PROCESSLIST %%i
+    call :SUB_PROCESSLIST %1 Report
 )
 popd
 endlocal
@@ -59,11 +57,21 @@ exit /b
 
 :SUB_PROCESSLIST
 if exist "%COMMON_PKG%\%1\customizations.xml" (
-    echo. Processing %1
-    cd %COMMON_PKG%\%1
-    call createprovpkg.cmd customizations.xml %1.ppkg > %PKGLOG_DIR%\%1.prov.log
-    if not errorlevel 0 ( echo. Error : Failed to create provisioning package. See %PKGLOG_DIR%\%1.prov.log )
+    set CUSTOMXMLPATH=%COMMON_PKG%\%1
+    set PPKGNAME=%1
+) else if exist "%PKGSRC_DIR%\%1\customizations.xml" (
+    set CUSTOMXMLPATH=%PKGSRC_DIR%\%1
+    set PPKGNAME=%1
+) else if exist "%PRODUCTS_DIR%\%1\prov\customizations.xml" (
+    set CUSTOMXMLPATH=%PRODUCTS_DIR%\%1\prov
+    set PPKGNAME=%1Prov
 ) else (
-    echo. Skipping %1
+    if [%2] == [Report] ( echo. Skipping %1 : customizations.xml not found )
+    exit /b
 )
+
+echo. Processing %1
+call createprovpkg.cmd %CUSTOMXMLPATH%\customizations.xml %CUSTOMXMLPATH%\%PPKGNAME%.ppkg > %PKGLOG_DIR%\%PPKGNAME%.prov.log
+if errorlevel 1 ( echo. Error : Failed to create %PPKGNAME%.ppkg. See %PKGLOG_DIR%\%PPKGNAME%.prov.log )
+
 exit /b
